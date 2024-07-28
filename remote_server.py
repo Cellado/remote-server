ab0a08d35dd5ee5050d5a477036469b3ee9b239e
Adin355 <adincaspary@gmail.com>
2024-07-28 18:19:51 -0400

    added server connection

--- remote_server.py
+++ remote_server.py
@@ -4,16 +4,16 @@ from signal import pause
 
 
 
-# def send_command(host, port, command):
-#     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-#         sock.connect((host, port))
-#         sock.sendall(command.encode())
-#         response = sock.recv(1024)
-#         print(response.decode('utf-8'))
+def send_command(host, port, command):
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+        sock.connect((host, port))
+        sock.sendall(command.encode())
+        response = sock.recv(1024)
+        print(response.decode('utf-8'))
 
-# if __name__ == "__main__":
-#     host = '192.168.1.48'
-#     port = 5138
+if __name__ == "__main__":
+    host = '192.168.1.48'
+    port = 5138
 
 PIN_A = 17
 PIN_B = 27
@@ -25,11 +25,13 @@ step_counter = 0
 def value_changed():
     global step_counter
     if encoder.steps > 0:
+        send_command(host, port, 'on')
         step_counter += 1
     else:
         step_counter -= 1
-    encoder.steps = 0
-    print("Counter: ", step_counter)
+        send_command(host, port, 'off')
+        encoder.steps = 0
+        print("Counter: ", step_counter)
 
 
 encoder.when_rotated = value_changed
